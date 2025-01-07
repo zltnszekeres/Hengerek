39d6526cbfe217a313bcfcdfdf4ae094876c3580
zltnszekeres <zltn.szekeres@yahoo.com>
2025-01-07 14:15:54 +0100

    cso

--- src/hengerek/Cso.java
+++ src/hengerek/Cso.java
@@ -2,6 +2,45 @@
 package hengerek;
 
 
-public class Cso {
+public class Cso extends TomorHenger {
+    private double falvast;
+
+    public Cso(double falvast, double sugar, double magassag, double fajsuly) {
+        super(sugar, magassag, fajsuly);
+        this.falvast = falvast;
+    }
+
+    public Cso(double falvast, double sugar, double magassag) {
+        this(falvast,sugar, magassag,1);
+        
+    }
     
+    public double getFalvastagsag() {
+        return falvast;
+    }
+    
+     @Override
+   public double terfogat(){
+
+    Henger segedHenger=new Henger(super.getSugar()-this.falvast, super.getMagassag());
+       Henger.hengerDarabCsokkent();
+        return super.terfogat()- segedHenger.terfogat();
+    }
+
+     @Override
+    public String toString() {
+        return "Cso{"+ "sugar=" + super.getSugar() + ", magassag=" + super.getMagassag()  + ", fajsuly=" + super.getFajsuly()  + ", falvastagsag=" + falvast + "}";
+    }
+
+   
+
+    
+
+   
+    
+
+    
+    
+    
+
 }
