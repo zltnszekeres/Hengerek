e036081b8f6baee4f65c600067263e8b339596ac
zltnszekeres <zltn.szekeres@yahoo.com>
2025-01-07 13:49:38 +0100

    tomorhenger

--- src/hengerek/TomorHenger.java
+++ src/hengerek/TomorHenger.java
@@ -2,6 +2,30 @@
 package hengerek;
 
 
-public class TomorHenger {
+public class TomorHenger extends Henger{
+    private double fajsuly;
+
+    public TomorHenger(double sugar, double magassag) {
+        this(sugar, magassag, 1);
+    }
+
+    public TomorHenger(double sugar, double magassag, double fajsuly) {
+        super(sugar, magassag);
+        this.fajsuly = fajsuly;
+    }
+
+    public double getFajsuly() {
+        return fajsuly;
+    }
+
+    @Override
+    public String toString() {
+        String os = super.toString();
+        return os + "\n MH<|--"
+                + "TomorHenger{" + "fajsuly=" + fajsuly + '}';
+    }
     
+    public double suly(){
+        return super.terfogat() * this.fajsuly;
+    }
 }
